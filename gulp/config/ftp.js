e184f754cfeec223112b34c02e67aecf77ddbb75
pgpa <32373425+pgpa@users.noreply.github.com>
2022-01-30 11:21:26 +0300

    Add files via upload

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+  host: 'xxx.beget.yyy', // Адрес FTP сервера
+  user: 'xxx_yyy', // Имя пользователя
+  password: 'xxx', // Пароль
+  parallel: 20 // Кол-во одновременных потоков
+}
